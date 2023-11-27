236c37af8d24ab3582a9280134a1fa775ccb93aa
Surge <esfloyd9341@gmail.com>
2023-11-27 13:47:06 -0500

    Add responsive CSS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,10 @@
-// A function that toggles a dropdown on the navbar using a specified id
-function toggleDropdown(id) {
-    var dropdown = document.getElementById(id);
-    if (dropdown.style.display === "none") {
-        dropdown.style.display = "block";
-    } else {
-        dropdown.style.display = "none";
+// Small screen navbar logic
+// --------------------------------------------------------
+
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
     }
-}
+);
